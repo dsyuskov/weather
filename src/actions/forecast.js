ad769c1fe20412faba7a4d1914c8563e71333114
int <yuskovss@gmail.com>
2019-11-21 00:11:21 +0400

    feat: add choise 3 days of forecast

--- src/actions/forecast.js
+++ src/actions/forecast.js
@@ -8,7 +8,6 @@ const API_KEY = 'APPID=d5ecba2b149b9cdfb1fea656c735177d';
 const API_SYSTEM = 'units=metric';
 const ID = 'id=';
 
-
 export const getWeatherForecastRequest = bool => {
   return {
     type: GET_WEATHER_FORECAST_REQUEST,
@@ -17,7 +16,6 @@ export const getWeatherForecastRequest = bool => {
 }
 
 export const getWeatherForecastSuccess = item => {
-  console.log(item);
   return {
     type: GET_WEATHER_FORECAST_SUCCESS,
     payload: item
@@ -31,11 +29,10 @@ export const getWeatherForecastFailture = bool => {
   }
 }
 
-
-export const getWeatherForecast = city => {
+export const getWeatherForecast = (city, ) => {
   return (dispatch) => {
     dispatch(getWeatherForecastRequest(true));
-    
+
     fetch(`${PATH_BASE}${FORECAST}?${API_KEY}&${API_SYSTEM}&${ID}${city}`)
       .then(response => {
         if (!response.ok) {
@@ -45,10 +42,39 @@ export const getWeatherForecast = city => {
         return response;
       })
       .then(response => response.json())
-      .then(item => {
+      .then(items => {
         dispatch(getWeatherForecastFailture(false));
-        dispatch(getWeatherForecastSuccess(item));
+        dispatch(getWeatherForecastSuccess(prepareForecast(items)));
       })
       .catch(() => dispatch(getWeatherForecastFailture(true)))
-      }
+    }
 }
+
+function prepareForecast(items) {
+  const HOURS_TO_DAY = 24;
+  const MINUTS_TO_HOURS = 3600;
+  const MILLISECONDS_TO_MINUTS = 1000;
+
+  const DAY = HOURS_TO_DAY * MINUTS_TO_HOURS * MILLISECONDS_TO_MINUTS;
+
+  const TODAY = new Date().setUTCHours(12, 0, 0, 0);
+
+  const NEXT_DAY = [];
+  NEXT_DAY.push(new Date(TODAY + DAY).getTime());
+  NEXT_DAY.push(new Date(TODAY + DAY * 2).getTime());
+  NEXT_DAY.push(new Date(TODAY + DAY * 3).getTime());
+
+  const forecastForThreeDays = items.list.filter((item) =>  NEXT_DAY.includes( item.dt * MILLISECONDS_TO_MINUTS ));
+
+  const result = {};
+
+  forecastForThreeDays.forEach( (item, i) => {
+    result[i] = {
+      weekDay: new Date(NEXT_DAY[i]).getDay(),
+      temp: item.main.temp,
+      icon: item.weather[0].icon,
+    };
+  });
+
+  return  result;
+};
