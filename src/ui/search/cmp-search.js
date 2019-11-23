d20de03bde96fc4ff13052ee15aaa167dc06ae2b
int <yuskovss@gmail.com>
2019-11-23 09:32:32 +0400

    faet: refactor class search to function search

--- src/ui/search/cmp-search.js
+++ src/ui/search/cmp-search.js
@@ -1,40 +1,24 @@
 import React from 'react';
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: '',
-    };
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onChange(event) {
-    this.setState({ search: event.target.value })
-  }
-
-  render() {
-    return (
-      <div className="search">
-        <form className="search__form">
-          <input 
-            className="search__input" 
-            type="text"
-            placeholder="Enter city name"
-            onChange={ this.onChange }
-            value={ this.state.search }
-          />
-          <button 
-            className="button search__button"
-            onClick={(event) => {
-              this.props.onClick(this.state.search);
-              event.preventDefault();
-            }}
-          >Search</button>
-        </form>
-      </div>
-    )
-  }
+export default function Search(props){
+  return (
+    <div className="search">
+      <form className="search__form">
+        <input 
+          className="search__input" 
+          type="text"
+          placeholder="Enter city name"
+          onChange={ props.onChange }
+          value={ props.value }
+        />
+        <button 
+          className="button search__button"
+          onClick={(event) => {
+            props.onClick();
+            event.preventDefault();
+          }}
+        >Search</button>
+      </form>
+    </div>
+  )
 }
-
-export default Search;
